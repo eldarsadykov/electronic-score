311953899d468ff1bac123f0bfff952299d2ddec
eldarsadykov <88986450+eldarsadykov@users.noreply.github.com>
2022-06-20 18:04:29 +0300

    created tvOS project

---
+++ electronic-score-exposition-tv-os/electronic-score-exposition-tv-os/electronic_score_exposition_tv_osApp.swift
@@ -0,0 +1,17 @@
+//
+//  electronic_score_exposition_tv_osApp.swift
+//  electronic-score-exposition-tv-os
+//
+//  Created by Эльдар Садыков on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct electronic_score_exposition_tv_osApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
