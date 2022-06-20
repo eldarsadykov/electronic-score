93517def800927b139111a858988829fd676dcb4
eldarsadykov <88986450+eldarsadykov@users.noreply.github.com>
2022-06-20 18:19:39 +0300

    triangle drawn

--- electronic-score-exposition-tv-os/electronic-score-exposition-tv-os/ContentView.swift
+++ electronic-score-exposition-tv-os/electronic-score-exposition-tv-os/ContentView.swift
@@ -7,10 +7,37 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    struct Triangle: Shape {
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            path.move(to: CGPoint(x: (rect.minX+0.5*rect.maxX), y: rect.minY))
+            
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            //path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.maxY),
+            //control: CGPoint(x: rect.midX, y: rect.midY))
+            
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            //path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.minY),
+            //control: CGPoint(x: rect.maxX, y: rect.midY))
+            
+            path.closeSubpath()
+            return path
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Triangle()
+                .stroke(lineWidth: 3)
+                .padding()
+            //.rotationEffect(Angle(degrees: 270))
+            //.frame(width: 300, height: 200)
+            
+            
+        }
+
     }
 }
 
