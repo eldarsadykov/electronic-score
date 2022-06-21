54998937ac685f70265d52e4c263ab1fb0d12f22
eldarsadykov <88986450+eldarsadykov@users.noreply.github.com>
2022-06-21 20:28:45 +0300

    basic fm synth and timeline visualisation

--- electronic-score-exposition-tv-os/electronic-score-exposition-tv-os/ContentView.swift
+++ electronic-score-exposition-tv-os/electronic-score-exposition-tv-os/ContentView.swift
@@ -53,7 +53,7 @@ class OscillatorObject {
         osc.$modulationIndex.ramp(to: 1, duration: 2)
     }
     func on(){
-        osc.$amplitude.ramp(to: 0.5, duration: 2)
+        osc.$amplitude.ramp(to: 0.1, duration: 2)
     }
     func off(){
         osc.$amplitude.ramp(to: 0, duration: 2)
@@ -61,41 +61,66 @@ class OscillatorObject {
 }
 
 struct ContentView: View {
-    struct Triangle: Shape {
+    struct Triangle: InsettableShape {
+        var insetAmount = 0.0
         func path(in rect: CGRect) -> Path {
             var path = Path()
-            path.move(to: CGPoint(x: (rect.minX+0.5*rect.maxX), y: rect.minY))
-            
-            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-            //path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.maxY),
-            //control: CGPoint(x: rect.midX, y: rect.midY))
-            
-            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-            //path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.minY),
-            //control: CGPoint(x: rect.maxX, y: rect.midY))
-            
+            let minX = rect.minX+insetAmount
+            let minY = rect.minY+insetAmount
+            let maxX = rect.maxX-insetAmount
+            let maxY = rect.maxY-insetAmount
+            path.move(to: CGPoint(x: (minX+0.5*maxX), y: minY))
+            path.addLine(to: CGPoint(x: minX, y: maxY*0.5))
+            path.addLine(to: CGPoint(x: maxX, y: maxY))
+//            path.addLine(to: CGPoint(x: (minX+0.5*maxX), y: minY))
             path.closeSubpath()
             return path
         }
+        func inset(by amount: CGFloat) -> some InsettableShape {
+            var arc = self
+            arc.insetAmount += amount
+            return arc
+        }
     }
     
-    var oscobj = OscillatorObject()
     
+    
+    var oscobj = OscillatorObject()
+    let baseWidth = 2.0
     var body: some View {
         VStack{
-            ZStack{                Rectangle()
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            GeometryReader {geometry in
+                ZStack{
+                    Rectangle()
+                        .strokeBorder(Color.gray, lineWidth: baseWidth)
+                        .opacity(0.5)
 
-                Triangle()
-                    .onAppear(){
-                        oscobj.start()
-                    }
-                    .onDisappear(){
-                        oscobj.stop()
-                    }
-                    .border(Color.white, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
-                    .padding(.all, 200.0)
+                    Triangle()
+                        .strokeBorder(Color.white,
+                                style: StrokeStyle(
+                                    lineWidth: baseWidth*2,
+                                    lineJoin: .round
+                                ))
+                        .onAppear(){
+                            oscobj.start()
+                        }
+                        .onDisappear(){
+                            oscobj.stop()
+                        }
 
+                    TimelineView(.animation) { context in
+                        let time = context.date.timeIntervalSinceReferenceDate
+                        let divider: TimeInterval = 20;
+                        let remainder = time.truncatingRemainder(dividingBy: divider)
+                        //                    Text("\(moduloTime)")
+                        let offset = geometry.size.width*((remainder-divider/2)/divider);
+                        Rectangle()
+                            .foregroundColor(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
+                            .offset(x: offset)
+                            .frame(width: baseWidth*5)
+                            .opacity(0.5)
+                    }
+                }
             }
             HStack{
                 VStack{
@@ -113,14 +138,10 @@ struct ContentView: View {
                     Button("Off") {
                         oscobj.off()
                     }
-                    
+
                 }
-                
+
             }
-            
-            
-            
-            
         }
         
     }
