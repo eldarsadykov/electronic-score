35f46feec04559be4e8d7649c8016656247b205c
eldarsadykov <88986450+eldarsadykov@users.noreply.github.com>
2022-06-29 16:32:07 +0300

    Removed file that shouldn't be tracked

---
+++ electronic-score-exposition-tv-os/electronic-score-exposition-tv-os/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  electronic-score-exposition-tv-os
+//
+//  Created by Эльдар Садыков on 29.06.2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
