ac8d3401cd27b5e9da4bccfd7a7daca9daa2814f
eldarsadykov <88986450+eldarsadykov@users.noreply.github.com>
2022-07-01 10:25:27 +0300

    Class Triangle Envelope – Node

---
+++ electronic-score-exposition-tv-os/electronic-score-exposition-tv-os/TriangleEnvelope.swift
@@ -0,0 +1,26 @@
+//
+//  TriangleEnvelope.swift
+//  electronic-score-exposition-tv-os
+//
+//  Created by Эльдар Садыков on 01.07.2022.
+//
+//
+//import AudioKit
+//import AVFoundation
+//import CSporthAudioKit
+//import Soundpipe
+//import SoundpipeAudioKit
+//import Sporth
+//import SporthAudioKit
+//import SwiftUI
+//
+//public class TriangleEnvelope: Node, Gated {
+//
+//    let input: Node
+//    let x1: AUValue
+//    let x2: AUValue
+//    let x3: AUValue
+//    init() {
+//
+//    }
+//}
